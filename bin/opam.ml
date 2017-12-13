6621d8681da9ab6f7a397870171c0f78e55bad53
Andrey Popp <8mayday@gmail.com>
2017-12-13 22:54:07 +0300

    Implement "opam config var prefix"

--- bin/opam.ml
+++ bin/opam.ml
@@ -1,1 +1,20 @@
+let error ?(code=1) msg =
+  Printf.eprintf "error: %s\n" msg;
+  exit code
+
+let () =
+
+  let cur__install =
+    try
+      Sys.getenv "cur__install"
+    with
+      Not_found ->
+        error "fauxopam is only available when running in Esy environment"
+  in
+
+  match Array.to_list Sys.argv with
+  | _::"config"::"var"::"prefix"::[] ->
+    print_endline cur__install
+  | _ ->
+    error "unsupported fauxpam invocation"
 
